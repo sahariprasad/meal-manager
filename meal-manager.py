6f5b81c3983e2c94189862d5b247db150044fe81
Hari Prasad SA <sahariprasad@gmail.com>
2022-07-06 23:03:30 +0530

    Added route for get all ingredients

--- meal-manager.py
+++ meal-manager.py
@@ -36,5 +36,27 @@ def find_food(item_list_string=None):
     return food_dict_str
 
 
+@app.route('/get_all_ingredients')
+def get_all_ingredients():
+    # read all recipes
+    recipe_path = "recipes"
+    recipe_files = [pos_json for pos_json in os.listdir(recipe_path) if pos_json.endswith('.json')]
+
+    recipe_dict = {}
+    for item in recipe_files:
+        recipe_name = item.replace(".json", "")
+        file = open(recipe_path + '/' + item, 'r', encoding='utf-8')
+        file_dict = json.load(file)
+        recipe_dict[recipe_name] = file_dict
+
+    ingredient_list = []
+    for item in recipe_dict:
+        ingredient_list += recipe_dict[item]["ingredients"]["mandatory"]
+    
+    ingredient_dict = {"ingredients": list(set(ingredient_list))}
+    ingredient_dict_str = str(ingredient_dict).replace("'", '"')
+    return ingredient_dict
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 1234)
