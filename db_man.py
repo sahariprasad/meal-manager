b2c438cd508519c58a808108a1c025af87019291
Padmavathy <56583057+Padmavathyn13@users.noreply.github.com>
2022-11-11 11:21:01 +0530

    Removed old connection string

--- db_man.py
+++ db_man.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
 def get_database():
-    # CONNECTION_STRING = "mongodb://useradmin:VelvetThunder.1993@localhost:27017"
     # CONNECTION_STRING = "mongodb://localhost:27017"
     CONNECTION_STRING = "mongodb://172.21.0.2:27017"
     client = MongoClient(CONNECTION_STRING)
